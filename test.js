d5a27265188846b0f97f8635720661bead3cb46d
akizuki <tooru.akizuki@gmail.com>
2015-02-22 16:19:09 +0900

    sample image

---
+++ test.js
@@ -0,0 +1,17 @@
+var im = require('imagemagick');
+
+var public_theta_dir='./public/images/theta/';
+var theta_imagepath=public_theta_dir + 'current.jpg';
+var speech_balloon_image_dir='./resource/';
+var speech_balloon_imagepath= speech_balloon_image_dir + 'speechballoon_gomi_nashi.png';
+var out_dir=public_theta_dir;
+var outfile=out_dir + 'out.jpg';
+
+im.convert([theta_imagepath, speech_balloon_imagepath, 
+'-gravity','northwest' '-geometry', '900x900+500+500','-composite',outfile], 
+function(err, metadata){
+	if (err) throw err;
+	console.log("end");
+});
+
+
