b706373ccc10af33107b45e114bad1eda50a015d
akizuki <tooru.akizuki@gmail.com>
2015-02-22 16:25:10 +0900

    sample image

--- test.js
+++ test.js
@@ -6,9 +6,10 @@ var speech_balloon_image_dir='./resource/';
 var speech_balloon_imagepath= speech_balloon_image_dir + 'speechballoon_gomi_nashi.png';
 var out_dir=public_theta_dir;
 var outfile=out_dir + 'out.jpg';
+var sb_pos='600x600-100+200';
 
 im.convert([theta_imagepath, speech_balloon_imagepath, 
-'-gravity','northwest' '-geometry', '900x900+500+500','-composite',outfile], 
+'-gravity','center','-geometry', sb_pos,'-composite',outfile], 
 function(err, metadata){
 	if (err) throw err;
 	console.log("end");
