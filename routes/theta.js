bac5815657e25778e412dd72319ba07340178fc2
akizuki <tooru.akizuki@gmail.com>
2015-02-22 16:45:11 +0900

    add speech balloon after save picture

--- routes/theta.js
+++ routes/theta.js
@@ -3,6 +3,13 @@ var router = express.Router();
 var Theta = require('ricoh-theta');
 var fs = require('fs');
 
+
+var im = require('imagemagick');
+var public_theta_dir='./public/images/theta/';
+var speech_balloon_image_dir='./resource/';
+var out_dir=public_theta_dir;
+
+
 var cronJob = require('cron').CronJob;
  
 // 毎秒実行
@@ -36,12 +43,28 @@ theta.on('connect', function(){
 
 // get picture
 theta.on('objectAdded', function(object_id){
-  theta.getPicture(object_id, function(err, picture){
-    fs.writeFile('public/images/theta/current.jpg', picture, function(err){
-      console.log('picture saved => .jpg');
-      theta.disconnect();
-    });
-  });
+	theta.getPicture(object_id, function(err, picture){
+		var savefilename='public/images/theta/current.jpg';
+		var temp_filename=savefilename+".temp.jpg";
+		fs.writeFile(temp_filename, picture, function(err){
+			console.log('picture saved => .jpg');
+			theta.disconnect();
+
+			//var theta_imagepath=public_theta_dir + 'current.jpg';
+			var theta_imagepath=temp_filename;
+			var speech_balloon_imagepath= speech_balloon_image_dir + 'speechballoon_gomi_nashi.png';
+			//var outfile=out_dir + 'out.jpg';
+			var outfile=savefilename;//overwrite
+			var sb_pos='600x600-100+200';//widthxheight+left+top(from center)
+
+			im.convert([theta_imagepath, speech_balloon_imagepath, 
+				'-gravity','center','-geometry', sb_pos,'-composite',outfile], 
+				function(err, metadata){
+					if (err) throw err;
+					console.log("save end");
+				});
+		});
+	});
 });
 
 }catch( e ){
